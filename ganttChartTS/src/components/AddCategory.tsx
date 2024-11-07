751666b87ebfae2dfee2130f513e3c3325de060b
Jion Choi <chlwldhs@gmail.com>
2024-11-07 13:52:27 -0700

    merged

--- ganttChartTS/src/components/AddCategory.tsx
+++ ganttChartTS/src/components/AddCategory.tsx
@@ -7,8 +7,13 @@ const AddCategory = () => {
 
     return (
         <>
+<<<<<<< HEAD
             <div className="add__container">
                 <i className="bi bi-plus icon-plus"></i>
+=======
+            <div className='add__container'>
+                <i className='bi bi-plus icon-plus'></i>
+>>>>>>> 64a6c37a005ae32ded4624c4383f5672b0866372
             </div>
         </>
     )
